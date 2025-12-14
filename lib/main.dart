406b31fce42f1c55f0d627ca736237096532c82d
倾听你的浪漫mm <>
2025-12-14 17:27:43 +0800

    无状态组件

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter 初体验1',
-    // theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
-    home: Scaffold(
-     appBar: AppBar(
-       title: Text('头部区域'),
-     ),
-     body: Container(
-      child: Center(
-       child: Text("中间区域"),
-      ),
-     ),
-     bottomNavigationBar: Container(
-      height: 50.0,
-      child: Center(
-       child: Text("底部区域2"),
-      ),
-     ),
-    ),
-  ));
+  runApp(
+    MainPage(),
+  );
 }
 
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter 初体验1',
+      home: Scaffold(
+        appBar: AppBar(title: Text('头部区域')),
+        body: Container(child: Center(child: Text("中间区域"))),
+        bottomNavigationBar: Container(
+          height: 80.0,
+          child: Center(child: Text("底部区域2")),
+        ),
+      ),
+    );
+  }
+}
