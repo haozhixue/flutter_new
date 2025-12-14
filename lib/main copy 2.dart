744dfe40f2d501020ee1da9baa3dab97f4944296
倾听你的浪漫mm <>
2025-12-14 17:42:05 +0800

    有状态组件

--- lib/main copy 2.dart
+++ lib/main copy 2.dart
@@ -5,30 +5,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: MyStatefulWidget(),
-      debugShowCheckedModeBanner: false,
-    ),
+    MainPage(),
   );
 }
 
-//创建一个有状态组件
-class MyStatefulWidget extends StatefulWidget {
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
 
-class   _MyStatefulWidgetState extends State<MyStatefulWidget> {
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      title: 'Flutter 初体验1',
+      home: Scaffold(
         appBar: AppBar(title: Text('头部区域')),
         body: Container(child: Center(child: Text("中间区域"))),
         bottomNavigationBar: Container(
           height: 80.0,
           child: Center(child: Text("底部区域2")),
         ),
-      );
+      ),
+    );
   }
 }
+
+
